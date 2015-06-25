be5220f958a85913d8f9414828ce01e01aba755e
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-06-25 16:32:53 +0200

    test create release

--- test.js
+++ test.js
@@ -98,3 +98,30 @@ test('get release by tag', function (t) {
     ]))
     .on('close'  , ghutils.verifyClose(t))
 })
+
+test('create release', function (t) {
+  t.plan(8)
+
+  var auth     = { user: 'authuser', token: 'authtoken' }
+    , org      = 'testorg'
+    , repo     = 'testrepo'
+    , testData = {
+          tag_name : '1.2.3-test'
+        , name     : 'Release name for 1.2.3-test'
+        , body     : 'Body text of release goes here'
+      }
+    , server
+
+  server = ghutils.makeServer(testData)
+    .on('ready', function () {
+      ghreleases.create(xtend(auth), org, repo, testData, ghutils.verifyData(t, testData))
+    })
+    .on('request', ghutils.verifyRequest(t, auth))
+    .on('post', ghutils.verifyUrl(t, [
+      'https://api.github.com/repos/' + org + '/' + repo + '/releases'
+    ]))
+    .on('post', function (url, data) {
+      t.deepEqual(data, testData)
+    })
+    .on('close'  , ghutils.verifyClose(t))
+})
